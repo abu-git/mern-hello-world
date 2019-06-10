c8ff32e91b679b069af386c2f8aab6fa6404b683
abu-git <abu.sammie@gmail.com>
2019-06-10 15:03:40 +0200

    deploy 2

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const appRoute = require('./routes/app');
 const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.use('/app', appRoute);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen(PORT, ()=>{
 	console.log("Server running on " + PORT);
